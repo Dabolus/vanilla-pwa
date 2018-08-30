cd3fa6d38584607edb4c3354cd30fb6719b92364
Giorgio Garasto <mail@dabolus.app>
2018-08-30 08:00:13 +0200

    feature: Add a basic precache manifest (we will be using it later on)

---
+++ precache-manifest.js
@@ -0,0 +1,16 @@
+self.precacheManifest = [
+  'index.html',
+  'site.webmanifest',
+  'components/base.js',
+  'components/utils.js',
+  'components/shared-styles.css',
+  'components/shell/element.js',
+  'components/shell/styles.css',
+  'components/shell/template.html',
+  'components/home/element.js',
+  'components/home/styles.css',
+  'components/home/template.html',
+  'components/info/element.js',
+  'components/info/styles.css',
+  'components/info/template.html',
+];

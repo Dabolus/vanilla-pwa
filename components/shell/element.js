561683245650de6e81ddc452641804baf58cb5fe
Giorgio Garasto <mail@dabolus.app>
2018-09-05 21:49:57 +0200

    feature: Add theme selector

--- components/shell/element.js
+++ components/shell/element.js
@@ -4,6 +4,7 @@ import { setupRouter } from '../utils.js';
 class MyShell extends BaseElement {
   constructor() {
     super('/components/shell/template.html', '/components/shell/styles.css');
+    this.setTheme(localStorage.getItem('theme') || 'light');
   }
 
   async connectedCallback() {
@@ -11,9 +12,19 @@ class MyShell extends BaseElement {
     setupRouter((location) => this.navigateTo(location));
     await this.readyPromise;
     this._updateNotification = this._root.querySelector('#update-notification');
+    this._themesDialog = this._root.querySelector('#themes-dialog');
     this._root.querySelector('#ignore').addEventListener('click', () => this.toggleUpdateNotification(false));
     this._root.querySelector('#update-now').addEventListener('click', () => this.update());
+    this._root.querySelector('#theme-selector').addEventListener('click', () => this.toggleThemesDialog());
+    this._themesDialog.addEventListener('click', () => this.toggleThemesDialog());
+    this._root.querySelectorAll('#themes > li')
+      .forEach((li) => li.addEventListener('click', () => this.setTheme(li.dataset.theme)));
     this.checkUpdates();
+  }
+
+  setTheme(theme) {
+    this.setAttribute('theme', theme);
+    localStorage.setItem('theme', theme);
   }
 
   async navigateTo({ pathname }) {
@@ -50,6 +61,11 @@ class MyShell extends BaseElement {
   toggleUpdateNotification(state) {
     const shouldShow = typeof state === 'undefined' ? !this._updateNotification.className : state;
     this._updateNotification.className = shouldShow ? 'active' : '';
+  }
+
+  toggleThemesDialog(state) {
+    const shouldShow = typeof state === 'undefined' ? !this._themesDialog.className : state;
+    this._themesDialog.className = shouldShow ? 'active' : '';
   }
 
   async checkUpdates() {

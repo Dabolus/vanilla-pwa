c2f8dc93a55aa86c12cc9a83a536a5b71ed187c3
Giorgio Garasto <giorgio@garasto.it>
2019-01-28 18:14:48 +0100

    feature: Use constructable stylesheets when possible

--- components/base.js
+++ components/base.js
@@ -1,22 +1,35 @@
+const supportsAdoptingStyleSheets = 'adoptedStyleSheets' in Document.prototype;
+
 class BaseElement extends HTMLElement {
   constructor(templateUrl, stylesUrl) {
     super();
-    this._fetchPromise = Promise.all([
+    this.readyPromise = Promise.all([
       fetch(templateUrl).then((res) => res.text()),
       fetch(stylesUrl).then((res) => res.text()),
       fetch('/components/shared-styles.css').then((res) => res.text()),
-    ]);
+    ]).then(([template, styles, sharedStyles]) => {
+      this._styles = `${sharedStyles} ${styles}`;
+      this._tmpl = document.createElement('template');
+      this._tmpl.innerHTML = `${supportsAdoptingStyleSheets ? '' : `<style>${this._styles}</style>`}${template}`;
+      this._root = this.attachShadow({ mode: 'open' });
+      if (supportsAdoptingStyleSheets) {
+        this.constructor.prototype.stylesheet = new CSSStyleSheet();
+        this.shadowRoot.adoptedStyleSheets = [this.constructor.prototype.stylesheet];
+      }
+      this._root.appendChild(this._tmpl.content.cloneNode(true));
+    });
   }
 
   connectedCallback() {
-    this.readyPromise =
-      this._fetchPromise
-        .then(([template, styles, sharedStyles]) => {
-          this._tmpl = document.createElement('template');
-          this._tmpl.innerHTML = `<style>${sharedStyles} ${styles}</style> ${template}`;
-          this._root = this.attachShadow({ mode: 'open' });
-          this._root.appendChild(this._tmpl.content.cloneNode(true));
-        });
+    this.readyPromise.then(() => {
+      if (
+        supportsAdoptingStyleSheets &&
+        this.constructor.prototype.stylesheet &&
+        this.constructor.prototype.stylesheet.cssRules.length === 0
+      ) {
+        return this.constructor.prototype.stylesheet.replace(this._styles);
+      }
+    });
   }
 }
 

3789deb1fff94c96b36ea6e7b4dc02b201874fe2
Giorgio Garasto <giorgio@garasto.it>
2019-03-13 22:53:14 +0100

    feature: Actually display sensors data

--- components/sensors/element.js
+++ components/sensors/element.js
@@ -3,52 +3,75 @@ import BaseElement from '../base.js';
 class MySensors extends BaseElement {
   constructor() {
     super('/components/sensors/template.html', '/components/sensors/styles.css');
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
     this.setupSensors();
   }
 
-  setupSensors() {
+  async setupSensors() {
+    await this.readyPromise;
     if ('LinearAccelerationSensor' in window) {
       const acceleration = new LinearAccelerationSensor({ frequency: 60 });
+      const accelerationData = this._root.querySelector('#acceleration-data');
       acceleration.addEventListener('reading', () =>
-        console.log(acceleration.x, acceleration.y, acceleration.z));
+        accelerationData.innerHTML =
+          `<strong>x:</strong> ${acceleration.x}<br>` +
+          `<strong>y:</strong> ${acceleration.y}<br>` +
+          `<strong>z:</strong> ${acceleration.z}`);
       acceleration.start();
     }
 
     if ('Gyroscope' in window) {
       const gyroscope = new Gyroscope();
+      const gyroscopeData = this._root.querySelector('#gyroscope-data');
       gyroscope.addEventListener('reading', () =>
-        console.log(gyroscope.x, gyroscope.y, gyroscope.z));
+        gyroscopeData.innerHTML =
+          `<strong>x:</strong> ${gyroscope.x}<br>` +
+          `<strong>y:</strong> ${gyroscope.y}<br>` +
+          `<strong>z:</strong> ${gyroscope.z}`);
       gyroscope.start();
     }
 
     if ('AbsoluteOrientationSensor' in window) {
       const absOrientation = new AbsoluteOrientationSensor();
-      absOrientation.onreading = () => console.log(absOrientation.quaternion);
+      const absOrientationData = this._root.querySelector('#abs-orientation-data');
+      absOrientation.addEventListener('reading', () =>
+        absOrientationData.innerHTML =
+          `<strong>quaternion:</strong> ${absOrientation.quaternion}`);
       absOrientation.start();
-      console.log(absOrientation);
     }
 
     if ('RelativeOrientationSensor' in window) {
       const relOrientation = new RelativeOrientationSensor();
+      const relOrientationData = this._root.querySelector('#rel-orientation-data');
       let rotationMatrix = new Float32Array(16);
-      relOrientation.onreading = () => {
+      relOrientation.addEventListener('reading', () => {
         relOrientation.populateMatrix(rotationMatrix);
-        console.log(rotationMatrix);
-      }
+        relOrientationData.innerHTML =
+          `<strong>rotation matrix:</strong> ${rotationMatrix}`;
+      });
       relOrientation.start();
     }
 
     if ('AmbientLightSensor' in window) {
       const ambientLight = new AmbientLightSensor();
+      const ambientLightData = this._root.querySelector('#ambient-light-data');
       sensor.addEventListener('reading', () =>
-        console.log(ambientLight.illuminance));
+        ambientLightData.innerHTML =
+          `<strong>illuminance:</strong> ${ambientLight.illuminance}`);
       ambientLight.start();
     }
 
     if ('Magnetometer' in window) {
       const magnetometer = new Magnetometer();
+      const magnetometerData = this._root.querySelector('#magnetometer-data');
       magnetometer.addEventListener('reading', () =>
-        console.log(magnetometer.x, magnetometer.y, magnetometer.z));
+        magnetometerData.innerHTML =
+          `<strong>x:</strong> ${magnetometer.x}<br>` +
+          `<strong>y:</strong> ${magnetometer.y}<br>` +
+          `<strong>z:</strong> ${magnetometer.z}`);
       magnetometer.start();
     }
   }
